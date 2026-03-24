eb1acd534e00d2a622300a0afad955034556813c
kams2004 <kamsuperold2004@gmail.com>
2026-03-24 21:16:57 +0100

    update

--- schedule-report.js
+++ schedule-report.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 
 console.log('⏰ Report scheduler started — will run daily at 20:47...');
 
-cron.schedule('47 20 * * *', () => {
+cron.schedule('18 21 * * *', () => {
   console.log(`\n🚀 [${new Date().toLocaleString()}] Running scheduled Wialon report...`);
   try {
     execSync('node wialon-report.js', { stdio: 'inherit', cwd: __dirname });

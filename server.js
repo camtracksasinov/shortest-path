d41d5e1d9dc4a190e9a3f1c49c542822aa11b9c2
kams2004 <kamsuperold2004@gmail.com>
2026-03-26 10:26:06 +0100

    updates structure

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ app.post('/shortest-path', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5458;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
